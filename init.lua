8970982a485e4db2e52cccf0a49326f552594718
Jannis <icken.jannis@gmail.com>
2025-10-12 16:06:18 +0200

    ignore latex in nvim-treesitter

--- init.lua
+++ init.lua
@@ -945,6 +945,7 @@ require('lazy').setup({
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ignore_install = { "latex" },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
